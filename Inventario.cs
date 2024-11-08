f65a2f5347465c3fa3ae322ffe2105fd166c16e2
Friederish <friederishmartinezramirez@gmail.com>
2024-11-08 07:39:43 -0600

    Add project files.

---
+++ Inventario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionDeInventario
+{
+    public class Inventario
+    {
+        private List<Producto> productos;
+
+
+        public Inventario()
+        {
+            productos = new List<Producto>();
+        }
+
+
+        public void AgregarProducto(Producto producto)
+        {
+            productos.Add(producto);
+        }
+
+        // Método para filtrar y ordenar los productos por precio mínimo
+        public IEnumerable<Producto> FiltrarYOrdenarProductos(decimal precioMinimo)
+        {
+            // Filtrar y ordenar productos con LINQ y expresiones Lambda
+            return productos
+               .Where(p => p.Precio > precioMinimo) 
+               .OrderBy(p => p.Precio); 
+        }
+    }
+}
